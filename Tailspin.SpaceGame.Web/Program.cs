db2c27c9d3be9fae11d20e3cc7d29ad751788e3e
synuhaanSogeti <sam.nuhaan@sogeti.com>
2024-07-15 11:20:11 +0200

    Return correct number of items

--- Tailspin.SpaceGame.Web/Program.cs
+++ Tailspin.SpaceGame.Web/Program.cs
@@ -12,6 +12,7 @@ namespace TailSpin.SpaceGame.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+                .UseUrls("https://localhost:801");
     }
 }
